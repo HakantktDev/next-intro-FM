69ac774c5f92a5e22a163b6c98951f07a498a918
HakantktDev <hakantokatdev@gmail.com>
2024-02-25 14:38:05 +0300

    server-components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-const Home = () => {
+const getData = async () => {
+  await new Promise((res) => setTimeout(() => res(0), 2000));
+  return { data: [1, 2, 3] };
+};
+
+const Home = async () => {
+  const data = await getData();
+  console.log(data);
   return <div>Home</div>;
 };
 export default Home;
