0631b4d20643e3829f68c26680e7f8d2ed22ff43
HakantktDev <hakantokatdev@gmail.com>
2024-02-27 22:54:17 +0300

    client-compoenents-and-prisma-configuration

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,4 +1,18 @@
-const TodoPage = () => {
-  return <div>Settings</div>;
+import db from '@/utils/db';
+import TodoList from '@/components/TodoList';
+
+const getData = async () => {
+  const todos = await db.todo.findMany({});
+  console.log(todos);
+  return todos;
+};
+
+const TodoPage = async () => {
+  const todos = await getData();
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  );
 };
 export default TodoPage;
